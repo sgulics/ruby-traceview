7fc3ec2315122d8cf343f5491cfd96f5ae98803e
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-05-13 18:46:24 +0200

    Name file according to contents

--- lib/oboe/collector_thread.rb
+++ lib/oboe/collector_thread.rb
@@ -2,9 +2,9 @@
 # All rights reserved.
 
 module Oboe
-  module CollectorThread < ::Thread
+  class CollectorThread < ::Thread
     def initialize(name)
-      Oboe.logger.debug "Creating CollectorThread: #{name}"
+      Oboe.logger.debug "[oboe/debug] Spawning CollectorThread: #{name}"
       super
     end
   end
