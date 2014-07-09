f2c1eb5e76ec0f75b1f3eaacaec1932fc8672208
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-07-09 23:32:29 +0200

    Support naming of Oboe threads.

--- lib/oboe/collector_thread.rb
+++ lib/oboe/collector_thread.rb
@@ -3,8 +3,11 @@
 
 module Oboe
   class CollectorThread < ::Thread
+    attr_reader :name
+
     def initialize(name)
       Oboe.logger.debug "[oboe/debug] Spawning CollectorThread: #{name}"
+      @name = name
       super
     end
   end
