cdb51fd6d10ae6d226dcc95f6283210145ae94e6
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-09-21 21:31:19 +0200

    Unicorn request queue depth reporting

---
+++ lib/oboe/collectors/unicorn.rb
@@ -0,0 +1,18 @@
+# Copyright (c) 2014 AppNeta, Inc.
+# All rights reserved.
+
+if defined?(::Unicorn)
+  Oboe.collector.register do
+    report_kvs = {}
+
+    if defined?(::Raindrops::Linux)
+      listener_addr = '0.0.0.0:' + ENV['PORT']
+      stats = Raindrops::Linux.tcp_listener_stats([ listener_addr ])[listener_addr]
+
+      report_kvs[:queued] = stats.queued
+      report_kvs[:active] = stats.active
+    end
+
+    report_kvs
+  end
+end
