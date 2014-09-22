7ec0bcda19de1050153458dd6029401ba14405a4
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-09-22 16:10:56 +0200

    Report Unicorn KVs for all potential listeners

--- lib/oboe/collectors/unicorn.rb
+++ lib/oboe/collectors/unicorn.rb
@@ -6,12 +6,16 @@ if defined?(::Unicorn)
     report_kvs = {}
 
     if defined?(::Raindrops::Linux)
-      listener_addr = '0.0.0.0:' + ENV['PORT']
-      stats = Raindrops::Linux.tcp_listener_stats([ listener_addr ])[listener_addr]
+      # Here we retrieve all Unicorn listeners (there may be more than one)
+      # and we report statistics for each.
+      listeners = Unicorn.listener_names
 
-      report_kvs[:addr] = Oboe::Util.getaddr
-      report_kvs[:queued] = stats.queued
-      report_kvs[:active] = stats.active
+      listeners.each_with_index do |listener_addr, i|
+        stats = Raindrops::Linux.tcp_listener_stats([ listener_addr ])[ listener_addr ]
+        report_kvs["listener#{i}_addr"] = listener_addr
+        report_kvs["listener#{i}_queued"] = stats.queued
+        report_kvs["listener#{i}_active"] = stats.active
+      end
     end
 
     report_kvs
