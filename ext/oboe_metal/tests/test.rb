0ea6792db48833d373c9a1943ed3468c441bc023
pglombardo <pglombardo@gmail.com>
2016-06-29 23:27:46 +0200

    Updated c-extension test

--- ext/oboe_metal/tests/test.rb
+++ ext/oboe_metal/tests/test.rb
@@ -1,11 +1,21 @@
 # Copyright (c) 2013 AppNeta, Inc.
 # All rights reserved.
 
-require 'oboe'
+require 'traceview'
 
-r = Oboe::UdpReporter.new('127.0.0.1')
-Oboe::Context.init()
-e = Oboe::Context.createEvent()
-e.addInfo("TestKey", "TestValue")
-r.sendReport(e)
+begin
+  r = TraceView::UdpReporter.new('127.0.0.1')
+  TraceView::Context.init()
+  e = TraceView::Context.createEvent()
+  e.addInfo("TestKey", "TestValue")
+  result = r.sendReport(e)
+
+  if result
+    puts "All appears well!"
+  else
+    puts "Reporter returned: #{result}"
+  end
+rescue => e
+  $stderr.puts e.inspect
+end
 
